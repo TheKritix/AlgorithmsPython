6209126bd414aabe265bd1e40a346b0a92eb2a79
Kristoffer <kristoffer@kritix.dk>
2022-02-10 09:34:11 +0100

    PythonTest

---
+++ PeakPoint.py
@@ -0,0 +1,6 @@
+# Python
+line = input() # Read line of input using input()
+input_number = int(input()) # Read line with one input integer
+multiple_numbers = list(map(int, input().split())) # Multiple integers on a line
+print(123) # Print result using print
+
